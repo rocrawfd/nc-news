c0bf74dfde4f50afbbf2e7c1957b9e7e455a4b48
Robert Crawford <robbie.crawford999@gmail.com>
2024-05-08 11:13:04 +0100

    imports router

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 import Header from './components/Header'
 import Articles from './components/Articles'
 import Searchbar from './components/Searchbar'
+import {Routes, Route} from "react-router-dom"
+
 
 function App() {
 
   const [articles, setArticles] = useState([])
 
   return (
-    <div>
+    <div className="HomeArticles">
       <Header />
       <Searchbar />
       <Articles articles={articles} setArticles={setArticles}/>
