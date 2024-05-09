4c5a3f39e9c7123af2cc8373d15f758dfdbe215e
Robert Crawford <robbie.crawford999@gmail.com>
2024-05-09 17:04:56 +0100

    adds ability to post comments

---
+++ src/components/AddComment.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+import { postComment } from "../../utils/utils"
+import { useParams } from 'react-router-dom'
+
+function AddComment({comments, setComments}){
+    const {article_id} = useParams()
+    const [hidden, setHidden] = useState(true)
+    const [className, setClassName] = useState("hidden")
+
+
+
+    function handleSubmit(event){
+        event.preventDefault()
+        const commentText = event.target[0].value
+        if(commentText.length !== 0){
+            return postComment(article_id, 'cooljmessy', commentText)
+            .then((newComment) => {
+                setClassName("hidden")
+                setComments([newComment, ...comments])
+            })
+          } else {setClassName("not-hidden")}
+        }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label for="typeComment">Type Comment:</label>
+            <input type="text" name="typeComment" id="typeComment" placeholder="type comment here..."/>
+            <p className={className}>comment cannot be blank</p>
+        <button>Add Comment</button>
+        </form>
+    )
+}
+
+export default AddComment
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom'
 import {useEffect, useState} from 'react'
 import axios from 'axios'
 import Comments from './Comments'
-import { upvoteArticle } from '../../../utils/utils'
+import { upvoteArticle } from '../../utils/utils'
 
 function Article(){
 
