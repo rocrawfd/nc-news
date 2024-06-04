16089df9edb9b1d40b8dd3b35acf4e8ae31a98fe
Robert Crawford <robbie.crawford999@gmail.com>
2024-06-04 16:49:17 +0100

    adds topics select and sort by bar

--- src/components/SortBy.jsx
+++ src/components/SortBy.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 export default function SortBy() {
+  
   return (
     <form>
         <label for='sortby-selector'>Sort By:</label>
-        <select></select>
+        <select name='sortby-selector' onChange={()=>{console.log('changed')}}>
+          <option>date</option>
+          <option>comments</option>
+          <option>votes</option>
+        </select>
     </form>
   )
 }
