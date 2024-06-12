62d427261ae5e2332afb3a7a865ec45784ed0e0a
Robert Crawford <robbie.crawford999@gmail.com>
2024-06-12 15:53:25 +0100

    adds link to github code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import {Link} from 'react-router-dom'
 
 function Header(){
     return (
-        <>
-        <h3>Logged in as cooljmessy</h3>
-     <Link to= '/' >Home</Link>
-     <h1 className="header">NC News</h1>
-        </>
+    <>
+    <h3>Logged in as cooljmessy</h3>
+    <Link to='https://github.com/rocrawfd/nc-news'>View the code on GitHub</Link>
+    <h1 className="header">NC News</h1>
+    </>
     )
 }
 
